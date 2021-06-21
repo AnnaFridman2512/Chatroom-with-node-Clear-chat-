03b12c364783ca3d75d91c18b3c63673f8098bd5
Anna-Fridman <annafridman81@gmail.com>
2021-06-21 13:15:16 +0300

    add new messages to DOM added

--- server.mjs
+++ server.mjs
@@ -1,57 +1,41 @@
-import http from 'http';
-import qs from 'querystringify';
+import express, { request, response } from 'express'; // the easy way to write a server 
 import fs from 'fs';
 
-let messagesString = fs.readFileSync('messages.json', {encoding: 'UTF-8'});//fs- comes with node, we don't need to install anything extra
-                                                                             //tels to read the 'messages.json' file which 
-                                                                             //is presented as an array (our choise)
-                                                                      
-const messages = JSON.parse(messagesString);//takes a string and returns an object
-
-const server = http.createServer(
-    (request, response) => { 
-        if(request.url === "/messages"){ //If the requested URL is "messages"
-            messagesString = fs.readFileSync('messages.json', {encoding: 'UTF-8'});//Reads the 'messages.json' file again
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(messagesString);//Show me the messages that are saved
-            response.end();
-            
-        } else {
-            const queryString = request.url.replace('/', '');//querystringify downloaded- returns urlrequest without / sign 
-                                                            //console.log(queryString); ---> (?name=Anna&message=bla+bla)
-            const params = qs.parse(queryString);//takes the string and returns an object
-                                                //console.log(params);---> { name: 'Anna', message: 'bla bla' }
-            if(params.message){//the 'message' is the input that we typed in, name. 
-                               //is what we get when typing the request (can see it in the url line in the broweser)({ name: 'Anna', message: 'bla bla' })
-                               messages.push({//we push the new object to the messagesString array 
-                               name: params.name,  
-                               message: params.message,
-                               date: new Date(),
-                               });
-
-                               fs.writeFileSync(//we write the object in the 'messages.json' file
-                               'messages.json', 
-                                JSON.stringify(messages), //we convert the object to a string (now we have an array of strings writen in the file)
-                               { encoding: 'UTF-8' }
-                                );
-
-                               }
-
-        
-        let HTMLString = fs.readFileSync('index.html', {encoding: 'UTF-8'});
+const app = express(); //Creating express app
+const messages = [];
 
 
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(HTMLString);
-        response.end();
-        }
+const html = fs.readFileSync('index.html', {encoding: "UTF-8"});//Use fs library to read "index.html"
+app.get('/', (request, response) => {//When asking for main page
+    
+    if(request.query.message){//We can access query string parameters with express. the "qury" is an object with all the data of query string
+                              //If a message was sent through a query string 
+        const messagesStr = fs.readFileSync('messages.json', {encoding: 'UTF-8'});//read 'messages.json' returns a string
+        const messages = JSON.parse(messagesStr);//takes the messages string and converts it to an object 
+
+        messages.push({//Push that message to the "messages" array (the message is an object)
+            name: request.query.name,
+            message: request.query.message,
+            date: new Date(),
+        });
+
+        fs.writeFileSync('messages.json', JSON.stringify(messages));//Replaces the content of 'messages.json' with 
+                                                                   //JSON.stringify - Takes the "messages" object and converts it to a string, 
+                                                    
     }
+    response.send(html);//return the html page
+
+});
+
+app.get('/messages', (request, response) => {  //When asking for 'messages'
+                                                    
+    const messages = fs.readFileSync('messages.json', {encoding: "UTF-8"}); //Read 'messages.json' file and return the messages as a string 
+    response.json(JSON.parse(messages));//JSON.parse - takes the messages string and converts it to an object 
+                                        //json() - returns promise which resolves with a JavaScript object that is the result of parsing the body text as JSON
+                                        //Note that despite the method being named json(), the result is not JSON 
+                                        //but is instead the result of taking JSON as input and parsing it to produce a JavaScript object.
+}
 );
 
-server.listen(8080);
+app.listen(8080); //Port
 
-console.log('Listening on: http://localhost:8080');
-
-// Statefull - A server with a running living state
-// Stateless - A server without a running state,
-//             state is saved in an external resource
